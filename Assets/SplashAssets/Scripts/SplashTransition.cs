db5463a83959c348b8963f9b8fc528b5dde3fc98
Edward Kim <buttereflay@Gmail.com>
2020-10-27 16:47:24 -0700

    init

---
+++ Assets/SplashAssets/Scripts/SplashTransition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashTransition : MonoBehaviour
+{
+    [SerializeField]
+    private float time;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadMainScreen());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LoadMainScreen()
+    {
+        yield return new WaitForSeconds(time);
+
+        SceneManager.LoadScene("MainScene");
+    }
+}
---
+++ Assets/SplashAssets/Scripts/Splash_levelChanger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Splash_levelChanger : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private int levelToLoad;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
